607af2901c6b0fea0267f2e4a9209ab3db40174e
rohkevin <kevinrohmail@gmail.com>
2021-07-23 15:15:02 -0400

    Add Month data header and monthly graph data, basic styling to visualize data better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import ChartContainer from './components/ChartContainer';
+import MonthData from './components/MonthData';
 import { GlobalProvider } from './context/GlobalContext';
 
 function App() {
   return (
     <GlobalProvider >
-      <div>
-        
+      <div id="app-container">
+        <MonthData />
+        <ChartContainer />
       </div>
     </GlobalProvider>
   );
