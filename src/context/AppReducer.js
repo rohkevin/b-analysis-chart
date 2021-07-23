77f6938ba17bb321ef69979efff99c01b6a3d686
rohkevin <kevinrohmail@gmail.com>
2021-07-23 12:47:12 -0400

    Add basic get data for budget, incomes, and spendings

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,23 @@
 export const AppReducer = (state, action) => {
   switch(action.type) {
+    case 'GET_BUDGET':
+      return {
+        ...state,
+        budget: action.payload
+      }
+
+    case 'GET_SPENDINGS':
+      return {
+        ...state,
+        spendings: action.payload
+      }
+    
+    case 'GET_INCOMES':
+      return {
+        ...state,
+        incomes: action.payload
+      }
+
     default: 
       return state;
   }
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,16 +1,52 @@
 import React, { createContext, useContext, useReducer } from 'react'
 import { AppReducer } from './AppReducer'
+import { data } from '../data.js'
 
 const initialState = {
-
+  budget: 0,
+  spendings: [],
+  incomes: [],
+  monthData: {spending: 0, income: 0}
 }
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  const getBudget = () => {
+    dispatch({
+      type: 'GET_BUDGET',
+      payload: data.overall_budget
+    })
+  }
+  const getSpendings = () => {
+    // Use try catch if getting data from server
+    dispatch({
+      type: 'GET_SPENDINGS',
+      payload: data.spending
+    })
+  }
+  const getIncomes = () => {
+    dispatch({
+      type: 'GET_INCOMES',
+      payload: data.income
+    })
+  }
+  const getMonthData = (month) => {
+    dispatch({
+      type: 'GET_MONTH_DATA',
+      payload: month
+    })
+  }
+
+
   return (
     <GlobalContext.Provider value ={{
+      budget: state.budget,
+      spendings: state.spendings,
+      incomes: state.incomes,
+      monthData: state.monthData,
+      getBudget, getSpendings, getIncomes, getMonthData,
 
     }}>
       { children }
